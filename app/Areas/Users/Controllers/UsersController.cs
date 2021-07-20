6b0d2626931990dc747469d2d895f2ba559771bd
mihaelcruz <mihael.cruz@gmail.com>
2021-07-20 19:24:40 -0400

    Agregar archivos de proyecto.

---
+++ app/Areas/Users/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app.Areas.Users.Controllers
+{
+    [Area("Users")]
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
